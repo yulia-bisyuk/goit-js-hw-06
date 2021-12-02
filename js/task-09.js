2705e949beaf062773a795172f7839f1b2c5d883
yulia-bisyuk <yulia.bisyuk@gmail.com>
2021-12-02 13:35:40 +0200

    Task 9 got done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
+
+const bodyEl = document.querySelector('body');
+const textEl = document.querySelector('.color');
+const buttonEl = document.querySelector('.change-color');
+
+buttonEl.addEventListener('click', onChangeColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function onChangeColor(event) {
+  const currentColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = currentColor;
+  textEl.textContent = currentColor;
+}
+
+
+
+
